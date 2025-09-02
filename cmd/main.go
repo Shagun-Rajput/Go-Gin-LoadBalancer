141a1e490bf1b8da6222e58bed2981b9031ad91b
Shagun Rajput <shagun.rajput@prod.hclpnp.com>
2025-09-02 20:00:22 +0530

    memory stats logged

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"Go-Gin-LoadBalancer/internal/router"
 	"github.com/gin-gonic/gin"
+	"runtime"
+	"time"
 )
 
 func main() {
@@ -11,6 +13,16 @@ func main() {
 
 	// Initialize routes from internal/router
 	router.SetupRoutes(r)
+
+	// Start goroutine to print memory usage every minute
+	go func() {
+		for {
+			var m runtime.MemStats
+			runtime.ReadMemStats(&m)
+			log.Printf("Memory Usage: Alloc = %v MiB, TotalAlloc = %v MiB, Sys = %v MiB, NumGC = %v", m.Alloc/1024/1024, m.TotalAlloc/1024/1024, m.Sys/1024/1024, m.NumGC)
+			time.Sleep(time.Minute)
+		}
+	}()
 
 	// Start server
 	if err := r.Run(); err != nil {
