8603521c158f79f89112fc7508ee6f97ea03052a
Shagun Rajput <shagun.rajput@prod.hclpnp.com>
2025-09-04 14:52:08 +0530

    logger for duration in place

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,11 +1,19 @@
 package router
 
 import (
-	"github.com/gin-gonic/gin"
-	"Go-Gin-LoadBalancer/internal/handler"
 	"Go-Gin-LoadBalancer/internal/config"
+	"Go-Gin-LoadBalancer/internal/handler"
+	"log"
+	"time"
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine, cfg config.ServerConfig) {
-	r.Any("/*path", handler.HandleRequest(cfg))
+	r.Any("/*path", func(c *gin.Context) {
+		start := time.Now()
+		log.Printf("***** Request received: %s %s", c.Request.Method, c.Request.URL.Path)
+		handler.HandleRequest(cfg)(c)
+		elapsed := time.Since(start).Seconds() * 1000
+		log.Printf("Response sent: %s %s | Duration: %.6f ms", c.Request.Method, c.Request.URL.Path, elapsed)
+	})
 }
