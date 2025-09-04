350a292128e86e8473959102c5c99fb644133262
Shagun Rajput <shagun.rajput@prod.hclpnp.com>
2025-09-04 15:34:06 +0530

    load balancer almost finalized 2

--- internal/router/router.go
+++ internal/router/router.go
@@ -11,7 +11,7 @@ import (
 func SetupRoutes(r *gin.Engine, cfg config.ServerConfig) {
 	r.Any("/*path", func(c *gin.Context) {
 		start := time.Now()
-		log.Printf("***** Request received: %s %s", c.Request.Method, c.Request.URL.Path)
+		log.Printf("***** Request received for : %s %s", c.Request.Method, c.Request.URL.Path)
 		handler.HandleRequest(cfg)(c)
 		elapsed := time.Since(start).Seconds() * 1000
 		log.Printf("Response sent: %s %s | Duration: %.3f ms", c.Request.Method, c.Request.URL.Path, elapsed)
